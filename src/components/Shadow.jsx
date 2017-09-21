c820db5bd9062cd071199756972ae34698fd842c
Thành Nguyen <thanhn@Administrators-MacBook-Pro.local>
2017-09-21 19:49:50 +0300

    Add popup for my gear icon. Fixed shadow. Add new icon

--- src/components/Shadow.jsx
+++ src/components/Shadow.jsx
@@ -6,46 +6,36 @@ import tinycolor from 'tinycolor2'
 
 const dimBlack = 'rgba(0,0,0, .25)'
 const dimmerBlack = 'rgba(0,0,0, .22)'
-
-const getSpread = (elevation: number): { first: string, second: string } => {
-  switch (elevation) {
-    case 2:
-      return {
-        first: '0 3px 6px',
-        second: '0 3px 6px'
-      }
-    case 3:
-      return {
-        first: '0 10px 20px',
-        second: '0 6px 6px '
-      }
-    case 4:
-      return {
-        first: '0 14px 28px',
-        second: '0 10px 10px'
-      }
-    case 5:
-      return {
-        first: '0 19px 38px',
-        second: '0 15px 12px'
-      }
-    default:
-      return {
-        first: '0px 1px 3px',
-        second: '0 1px 2px'
-      }
-  }
+const shadows = [
+  '0px 1px 3px 0px rgba(91, 134, 229, 0.4),0px 1px 1px 0px rgba(91, 134, 229, 0.28),0px 2px 1px -1px rgba(91, 134, 229, 0.24)',
+  '0px 1px 5px 0px rgba(91, 134, 229, 0.4),0px 2px 2px 0px rgba(91, 134, 229, 0.28),0px 3px 1px -2px rgba(91, 134, 229, 0.24)',
+  '0px 1px 8px 0px rgba(91, 134, 229, 0.4),0px 3px 4px 0px rgba(91, 134, 229, 0.28),0px 3px 3px -2px rgba(91, 134, 229, 0.24)',
+  '0px 2px 4px -1px rgba(91, 134, 229, 0.4),0px 4px 5px 0px rgba(91, 134, 229, 0.28),0px 1px 10px 0px rgba(91, 134, 229, 0.24)',
+  '0px 3px 5px -1px rgba(91, 134, 229, 0.4),0px 5px 8px 0px rgba(91, 134, 229, 0.28),0px 1px 14px 0px rgba(91, 134, 229, 0.24)',
+  '0px 3px 5px -1px rgba(91, 134, 229, 0.4),0px 6px 10px 0px rgba(91, 134, 229, 0.28),0px 1px 18px 0px rgba(91, 134, 229, 0.24)',
+  '0px 4px 5px -2px rgba(91, 134, 229, 0.4),0px 7px 10px 1px rgba(91, 134, 229, 0.28),0px 2px 16px 1px rgba(91, 134, 229, 0.24)',
+  '0px 5px 5px -3px rgba(91, 134, 229, 0.4),0px 8px 10px 1px rgba(91, 134, 229, 0.28),0px 3px 14px 2px rgba(91, 134, 229, 0.24)',
+  '0px 5px 6px -3px rgba(91, 134, 229, 0.4),0px 9px 12px 1px rgba(91, 134, 229, 0.28),0px 3px 16px 2px rgba(91, 134, 229, 0.24)',
+  '0px 6px 6px -3px rgba(91, 134, 229, 0.4),0px 10px 14px 1px rgba(91, 134, 229, 0.28),0px 4px 18px 3px rgba(91, 134, 229, 0.24)',
+  '0px 6px 7px -4px rgba(91, 134, 229, 0.4),0px 11px 15px 1px rgba(91, 134, 229, 0.28),0px 4px 20px 3px rgba(91, 134, 229, 0.24)',
+  '0px 7px 8px -4px rgba(91, 134, 229, 0.4),0px 12px 17px 2px rgba(91, 134, 229, 0.28),0px 5px 22px 4px rgba(91, 134, 229, 0.24)',
+  '0px 7px 8px -4px rgba(91, 134, 229, 0.4),0px 13px 19px 2px rgba(91, 134, 229, 0.28),0px 5px 24px 4px rgba(91, 134, 229, 0.24)',
+  '0px 7px 9px -4px rgba(91, 134, 229, 0.4),0px 14px 21px 2px rgba(91, 134, 229, 0.28),0px 5px 26px 4px rgba(91, 134, 229, 0.24)',
+  '0px 8px 9px -5px rgba(91, 134, 229, 0.4),0px 15px 22px 2px rgba(91, 134, 229, 0.28),0px 6px 28px 5px rgba(91, 134, 229, 0.24)',
+  '0px 8px 10px -5px rgba(91, 134, 229, 0.4),0px 16px 24px 2px rgba(91, 134, 229, 0.28),0px 6px 30px 5px rgba(91, 134, 229, 0.24)',
+  '0px 8px 11px -5px rgba(91, 134, 229, 0.4),0px 17px 26px 2px rgba(91, 134, 229, 0.28),0px 6px 32px 5px rgba(91, 134, 229, 0.24)',
+  '0px 9px 11px -5px rgba(91, 134, 229, 0.4),0px 18px 28px 2px rgba(91, 134, 229, 0.28),0px 7px 34px 6px rgba(91, 134, 229, 0.24)',
+  '0px 9px 12px -6px rgba(91, 134, 229, 0.4),0px 19px 29px 2px rgba(91, 134, 229, 0.28),0px 7px 36px 6px rgba(91, 134, 229, 0.24)',
+  '0px 10px 13px -6px rgba(91, 134, 229, 0.4),0px 20px 31px 3px rgba(91, 134, 229, 0.28),0px 8px 38px 7px rgba(91, 134, 229, 0.24)',
+  '0px 10px 13px -6px rgba(91, 134, 229, 0.4),0px 21px 33px 3px rgba(91, 134, 229, 0.28),0px 8px 40px 7px rgba(91, 134, 229, 0.24)',
+  '0px 10px 14px -6px rgba(91, 134, 229, 0.4),0px 22px 35px 3px rgba(91, 134, 229, 0.28),0px 8px 42px 7px rgba(91, 134, 229, 0.24)',
+  '0px 11px 14px -7px rgba(91, 134, 229, 0.4),0px 23px 36px 3px rgba(91, 134, 229, 0.28),0px 9px 44px 8px rgba(91, 134, 229, 0.24)',
+  '0px 11px 15px -7px rgba(91, 134, 229, 0.4),0px 24px 38px 3px rgba(91, 134, 229, 0.28),0px 9px 46px 8px rgba(91, 134, 229, 0.24)'
+]
+const getSpread = (elevation: number) => {
+  return shadows[elevation < 0 || elevation > 24 ? 0 : elevation]
 }
 
 export const Paper = styled.div`
-  box-shadow: ${props => getSpread(props.elevation).first}
-      ${props =>
-        tinycolor(props.shadowColor)
-          .setAlpha(0.25)
-          .toRgbString() || dimBlack},
-    ${props => getSpread(props.elevation).second}
-      ${props =>
-        tinycolor(props.shadowColor)
-          .setAlpha(0.22)
-          .toRgbString() || dimmerBlack};
+  box-shadow: ${props => getSpread(props.elevation)};
 `
