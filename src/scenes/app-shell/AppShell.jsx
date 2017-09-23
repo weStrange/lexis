f98168387fd2ec2f7896761b9f7c5a4f1aa3b8e2
Thành Nguyen <thanhn@Administrators-MacBook-Pro.local>
2017-09-23 23:47:06 +0300

    Fix more style

--- src/scenes/app-shell/AppShell.jsx
+++ src/scenes/app-shell/AppShell.jsx
@@ -4,8 +4,8 @@ import SideNavigation from './SideNavigation'
 import styled from 'styled-components'
 
 const Content = styled.div`
-  width: calc(100vw - 250px);
-  height: calc(100vh - 88px);
+  width: calc(100% - 265px);
+  height: calc(100% - 88px);
   transform: translate3d(250px, 100px, 0);
 `
 
